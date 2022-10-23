6a754735ae597845bcfcdecb1881da1b2407a99a
Ivan Drahoner <idrahoner@gmail.com>
2022-10-23 21:54:16 +0300

    Completed 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const playerEl = document.querySelector('#vimeo-player');
+
+const PLAYBACK_TIME = 'videoplayer-current-time';
+
+const player = new Player(playerEl);
+
+player.on('play', onStartVideo);
+
+function onStartVideo() {
+  const lastTime = localStorage.getItem(PLAYBACK_TIME);
+  if (lastTime) {
+    player.setCurrentTime(lastTime);
+  }
+  player.on('timeupdate', throttle(onPlayVideo, 1000));
+}
+
+function onPlayVideo() {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem(PLAYBACK_TIME, time);
+  });
+}
