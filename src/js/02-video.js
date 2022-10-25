4794e27f417d3736763c7cef1fd352fd0e0d9cfb
Ivan Drahoner <idrahoner@gmail.com>
2022-10-25 21:38:15 +0300

    Refactored according to mintor's marks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,17 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-const playerEl = document.querySelector('#vimeo-player');
-
 const PLAYBACK_TIME = 'videoplayer-current-time';
+const previousTime = localStorage.getItem(PLAYBACK_TIME);
 
+const playerEl = document.querySelector('#vimeo-player');
 const player = new Player(playerEl);
 
-player.on('play', onStartVideo);
-player.on('timeupdate', throttle(onPlayVideo, 1000));
-
-function onStartVideo() {
-  const previousTime = localStorage.getItem(PLAYBACK_TIME);
-  if (previousTime) {
-    player.setCurrentTime(previousTime);
-  }
+if (previousTime) {
+  player.setCurrentTime(previousTime);
 }
+
+player.on('timeupdate', throttle(onPlayVideo, 1000));
 
 function onPlayVideo() {
   player.getCurrentTime().then(time => {
